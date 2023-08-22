c423b62305aa1e15e74125471f56826c4f2ed20c
NiniOak <anita.akaeze@hashicorp.com>
2023-08-22 16:25:22 -0700

    clean up PR

--- .github/scripts/filter_changed_files_go_test.sh
+++ .github/scripts/filter_changed_files_go_test.sh
@@ -4,7 +4,7 @@
 files_to_check=$(git diff --name-only origin/$GITHUB_BASE_REF)
 
 # Define the directories to check
-skipped_directories=("docs/" "ui/" "website/" "grafana/" ".github/")
+skipped_directories=("docs/" "ui/" "website/" "grafana/")
 
 # Initialize a variable to track directories outside the skipped ones
 other_directories=""
@@ -22,12 +22,12 @@ for file_to_check in $files_to_check; do
   if [ "$file_is_skipped" = "false" ]; then
     other_directories+="$(dirname "$file_to_check")\n"
 		trigger_ci=true
-		echo "One or more non doc file(s) changed: true"
+		echo "One or more non doc file(s) changed: $trigger_ci"
     echo -e $other_directories
     echo "trigger-ci=$trigger_ci" >> "$GITHUB_OUTPUT"
 		exit 0 ## if file is outside of the skipped_directory exit loop
   fi
 done
 
-echo "One or more non doc file(s) changed: false"
+echo "One or more non doc file(s) changed: $trigger_ci"
 echo "trigger-ci=$trigger_ci" >> "$GITHUB_OUTPUT"
